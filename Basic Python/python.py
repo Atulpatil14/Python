b629e84cdb508bd9a682534a60e2e7b76326ab7f
It's_Atul <patilatul1405@gmail.com>
2024-06-09 23:51:43 +0530

    Create python.py

---
+++ Basic Python/python.py
@@ -0,0 +1,15 @@
+name = input("Enter name")
+age = int(input("Enter age:"))
+print(name)
+
+#auto space no +
+print("my name is" + name)
+
+#auto space given ,
+print("my name is" , name)
+
+#new line and manually give sapce
+print("my name is",name,"\n I Love Rutu's Eyes!")
+
+#assign predefine  placeholder 
+print("my name is {} and my age is {}".format(name,age))
